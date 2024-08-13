addf8293e3bc3a9b33857360f521b8652c72dcf0
Fernando Sinaga <fernandosinaga427@gmail.com>
2024-08-13 16:28:20 +0700

    Projek akhir dts 1

---
+++ app/src/main/java/com/example/projekakhirdts/splashscreen.java
@@ -0,0 +1,36 @@
+package com.example.projekakhirdts;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class splashscreen extends AppCompatActivity {
+
+    // membuat timer menjadi 5 detik
+    private static int splash_timer= 5000;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_splashscreen);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                //setelah splashscreen abis lanjut ke login
+                Intent intent = new Intent(splashscreen.this,Login.class);
+                startActivity(intent);
+            }
+        },splash_timer);
+    }
+}
